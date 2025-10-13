9cc32eb69b4de12a60d5eece53c5fcdbb1dfad37
sephclt <josephajero22@gmail.com>
2025-10-13 23:18:13 +0800

    feat: added frontend files and initial nextjs setup

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,9 @@
+import { Center, Text } from "@mantine/core";
+
+export default function Home() {
+  return (
+    <Center w={"100vw"} h={"100vh"}>
+      <Text>Hello from AMS</Text>
+    </Center>
+  );
+}
