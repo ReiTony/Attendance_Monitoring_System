35f83044e9bf9690446000528773d82a80065541
sephclt <josephajero22@gmail.com>
2025-10-21 00:05:38 +0800

    feat: updated landing page to include login and signup buttons

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,18 @@
-import { Center, Text } from "@mantine/core";
+import { Button, Center, Stack, Text } from "@mantine/core";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <Center w={"100vw"} h={"100vh"}>
-      <Text>Hello from AMS</Text>
+      <Stack>
+        <Text>Attendance Monitoring System</Text>
+        <Button component={Link} href={"/login"}>
+          Login
+        </Button>
+        <Button component={Link} href={"/register"}>
+          Register
+        </Button>
+      </Stack>
     </Center>
   );
 }
