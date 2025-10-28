c1957877ea39b4de704c66170a04809d5be0a1bf
sephclt <josephajero22@gmail.com>
2025-10-28 13:29:12 +0800

    fix: remove register button in login form, add a back button to login page

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -1,10 +1,16 @@
-import { Center } from "@mantine/core";
+import { Button, Center, Stack } from "@mantine/core";
 import TeacherLoginForm from "./TeacherLoginForm";
+import Link from "next/link";
 
 export default function Login() {
   return (
     <Center w={"100vw"} h={"100vh"}>
-      <TeacherLoginForm />
+      <Stack gap={"lg"}>
+        <Button component={Link} href={"/"} maw={150}>
+          Back
+        </Button>
+        <TeacherLoginForm />
+      </Stack>
     </Center>
   );
 }
